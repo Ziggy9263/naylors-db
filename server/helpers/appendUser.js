5a2a36df192df4d6382bd55f7e69d05c2cbb5911
Ziggy9263 <midaz90@gmail.com>
2021-02-11 13:21:57 -0600

    Bug found, fixing.

--- server/helpers/appendUser.js
+++ server/helpers/appendUser.js
@@ -2,7 +2,7 @@ const User = require('../user/user.model');
 
 module.exports = appendUser = async (req, res, next) => {
   let auth = req.auth;
-  let user = User.get({uid: auth.uid}).then(doc => doc).catch(e => res.status(403).send('Unauthorized'));
+  let user = User.get({uid: auth.uid}).then(doc => doc).catch(e => res.status(403).send('Unauthorized, ' + e.toString()));
   req.user = user;
   next();
   return;
